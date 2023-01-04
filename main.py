00b9e01f40953ecc097270a21d3a2905fbe4c077
Nazarii <84344775+cWillNeverDie@users.noreply.github.com>
2023-01-04 18:28:39 +0200

    Minor update
    Code is structurized and now with comments. Upgraded readability, replaced from number of files in directory to number of .tess files in directory, added simple progress indicator

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 
+
 def split(name, *positions):
+    # Output will contain parts of the main file 
     output = []
+    
+    # Open mail file and split it
     with open(name, 'r') as file:
         content = file.read().splitlines()
         start = 0
         for i in positions:
             for position in i:
+                # Move part before gap into output
                 output.append(content[start:position])
                 start = position
+        # Move the last part 
         output.append(content[start:])
+    
+    
     counter = 1
     for out in output:
+        # Create files and write these parts
         with open(f"{name.split('.')[0]}_part{counter}.tess", 'w') as file:
             for line in out:
                 file.write(f"{line}\n")
@@ -19,23 +28,38 @@ def split(name, *positions):
     return
 
 
-files = os.listdir()
-print(f"{len(files)} files in directory")
-for file in files:
-    if 'tess' in file:
-        print(f"Checking {file}")
+if __name__ == '__main__':
+    # Check for .tess files
+    files = os.listdir()
+    tess_files = []
+    for file in files: 
+        if '.tess' in file:
+            tess_files.append(file)
+    print(f"{len(tess_files)} .tess files in this directory")
+
+
+    for file in tess_files:
+        print(f"Checking {file}", end=" ")
+        # Open every .tess file
         with open(file, 'r') as f:
             prev, temp = map(float, f.readline().split())
             counter = 0
             doINeedToSplit = False
             whereDoINeedToSplit = []
+            # Look for gaps in the data
             for line in f:
                 time, temp = map(float, line.split())
-                counter += 1
+                counter += 1  
+                # If your observation interval is quite big (day or more), change 1 to bigger number
                 if time - prev > 1:
                     doINeedToSplit = True
                     whereDoINeedToSplit.append(counter)
-                prev = time
+                prev = time 
+            
+            # Split the file if required
             if doINeedToSplit:
                 split(file, whereDoINeedToSplit)
+        print("done")
+
+
 
